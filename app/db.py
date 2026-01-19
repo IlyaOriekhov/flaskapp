f031af91ae0b36a87e5a237afd9d76ebd6988e87
IlliaOriekhov <ilya.orekhov@ukr.net>
2026-01-19 20:08:04 +0200

    make alembic migrations

--- app/db.py
+++ app/db.py
@@ -1,10 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, ForeignKey, MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
 
 db = SQLAlchemy(model_class=Base)
@@ -13,7 +21,23 @@ db = SQLAlchemy(model_class=Base)
 class Expense(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str]
     amount: Mapped[float] = mapped_column()
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    
+    user: Mapped["User"] = relationship(back_populates="expenses")
+
 
     def __repr__(self):
         return f"Expense(title='{self.title}', amount={self.amount})"
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] 
+    password: Mapped[str]
+    
+    expenses: Mapped[list["Expense"]] = relationship(back_populates="user")
+ 
+
+    def __repr__(self):
+        return f"User(username='{self.username}')"
